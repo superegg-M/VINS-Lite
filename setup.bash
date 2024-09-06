ce02666e650f4cbe26b2ad3bf9b57873fbb0f07b
cain <cain_hsui@gmail.com>
2024-09-06 15:27:17 +0800

    修改了setup.bash

--- setup.bash
+++ setup.bash
@@ -1,1 +1,4 @@
+echo performance > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
+echo performance > /sys/devices/system/cpu/cpufreq/policy4/scaling_governor
+echo performance > /sys/devices/system/cpu/cpufreq/policy6/scaling_governor
 export GOMP_CPU_AFFINITY="4-7"
